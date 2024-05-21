179823a7724813c114c858eafe2b3dc73ef59be8
s28210 <s28210@pjwstk.edu.pl>
2024-05-21 02:29:15 +0200

    testing LINQ tasks in LinqTasks.cs and Program.cs

---
+++ LinqTutorials/Models/Dept.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace LinqTutorials.Models
+{
+    public class Dept
+    {
+        public int Deptno { get; set; }
+        public string Dname { get; set; }
+        public string Loc { get; set; }
+        public List<Emp> Emps { get;  set; }
+    }
+}
